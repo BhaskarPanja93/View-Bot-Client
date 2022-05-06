f752de2e71c930579fbb977791ff5e0e3c627a86
Bhaskar Panja <bhaskarpanja93@gmail.com>
2022-05-07 05:13:38 +0530

    Changed data transfer mechanism, 2x-3x faster now.

--- user_login.py
+++ user_login.py
@@ -1,47 +1,48 @@
 import socket
 from os import system as system_caller
 from platform import system
+from time import sleep
+from random import choice
 
 BUFFER_SIZE = 1024*100
 host_ip, host_port = '', ''
 full_file_path = __file__.replace('\\','/')
 current_file_name = full_file_path.split('/')[-1].replace('.py','')
 
-def force_connect_server(type_of_connection):
+def force_connect_server():
     global host_ip, host_port
-    if type_of_connection == 'tcp':
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    else:
-        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.settimeout(10)
     while True:
         try:
             connection.connect((host_ip, host_port))
             break
         except:
+            sleep(2)
             from requests import get
-            text = get('https://bhaskarpanja93.github.io/AllLinks.github.io/').text.split('<p>')[-1].split('</p>')[0].replace('‘', '"').replace('’', '"').replace('“', '"').replace('”', '"')
+            text = get('https://bhaskarpanja93.github.io/AllLinks.github.io/').text.split('<p>')[-1].split('</p>')[0].replace('‘', '"').replace('’', '"').replace('“', '"').replace('”', '"').replace('<br>', '').replace('\n', '')
             link_dict = eval(text)
-            host_ip, host_port = link_dict['adfly_user_tcp_connection'].split(':')
+            user_connection_list = link_dict['adfly_user_tcp_connection_list']
+            host_ip, host_port = choice(user_connection_list).split(':')
             host_port = int(host_port)
     return connection
 
 
+
 def __send_to_connection(connection, data_bytes: bytes):
     data_byte_length = len(data_bytes)
-    connection.send(str(data_byte_length).encode())
-    if connection.recv(1) == b'-':
-        connection.send(data_bytes)
-    if connection.recv(1) == b'-':
-        return
+    connection.send(f'{data_byte_length}'.zfill(8).encode())
+    connection.send(data_bytes)
 
 
 def __receive_from_connection(connection):
-    length = int(connection.recv(BUFFER_SIZE))
-    connection.send(b'-')
+    length = b''
+    while len(length) != 8:
+        length+=connection.recv(8-len(length))
+    length = int(length)
     data_bytes = b''
     while len(data_bytes) != length:
-        data_bytes += connection.recv(BUFFER_SIZE)
-    connection.send(b'-')
+        data_bytes += connection.recv(length-len(data_bytes))
     return data_bytes
 
 
@@ -52,7 +53,7 @@ def clear_screen():
         system_caller('clear')
 
 
-self_update_connection = force_connect_server('tcp')
+self_update_connection = force_connect_server()
 __send_to_connection(self_update_connection, b'8')
 user_login_data = __receive_from_connection(self_update_connection)
 if open(full_file_path, 'rb').read() != user_login_data:
@@ -63,7 +64,7 @@ if open(full_file_path, 'rb').read() != user_login_data:
         import_module(current_file_name)
 
 
-login_connection = force_connect_server('tcp')
+login_connection = force_connect_server()
 __send_to_connection(login_connection, b'9')
 print("""Enter 0 to Login
 Enter 1 to Create a new account 
@@ -99,7 +100,7 @@ def password_matches_standard(password: str):
 
 def post_login_function():
     clear_screen()
-    print(f"Login successful, Welcome back {u_name}")
+    print(f"Login successful, Welcome {u_name}")
     while True:
         print("""
             Enter 1 to add new adfly ids to your account
@@ -146,7 +147,7 @@ def post_login_function():
                     break
                 old_ids = __receive_from_connection(login_connection).decode()
                 print(f"Your previously added ID are: {old_ids.replace(' ', ', ')}")
-                _id = input("Enter a single ID you want to add to your account or type x to return to previous page: ").strip()
+                _id = input("Enter a single ID you want to remove from your account or type x to return to previous page: ").strip()
                 if _id.lower() == 'x':
                     __send_to_connection(login_connection, b'x')
                     clear_screen()
