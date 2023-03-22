d932eaba54a001b4ce4d9c9554713fb04ad20a66
Bhaskar Panja <bhaskarpanja93@gmail.com>
2023-03-22 15:35:39 +0530

    v2.5.1
    [NEW] User Host now automatically opens web browser and takes you to new version page.
    [CHANGE] Added some new debug statements to better understand what went wrong if any issue occurs.
    [CHANGE] Increase server request timeouts, to meet the needs of slower internet connections.
    [FIX] fixed: #22
    Code will ignore rate limit responses and keep trying till it succeeds.

--- user_host_main.py
+++ user_host_main.py
@@ -1,15 +1,16 @@
-user_host_main_version = '2.5.0'
+user_host_main_version = '2.5.1'
 from threading import Thread
 from random import choice
 from requests import get
 from time import sleep
 from os import makedirs, path, system as system_caller
+import webbrowser
 
 
 def fetch_global_addresses():
     while True:
         try:
-            response = get("https://raw.githubusercontent.com/BhaskarPanja93/AllLinks.github.io/master/README.md", timeout=10)
+            response = get("https://raw.githubusercontent.com/BhaskarPanja93/AllLinks.github.io/master/README.md", timeout=15)
             response.close()
             link_dict = eval(response.text)
             global_host_page = choice(link_dict['viewbot_global_host_page_list'])
@@ -17,19 +18,24 @@ def fetch_global_addresses():
             global_host_address[-1] = int(global_host_address[-1])
             global_host_address = tuple(global_host_address)
             break
-        except Exception:
-            print("Recheck internet connection?")
+        except Exception as e:
+            print(f"Unable to connect to GitHub : {repr(e)}")
             sleep(0.1)
     return global_host_address, global_host_page
 
 
-## Check self version
 system_caller('cls')
+## Check self version
 while True:
     global_host_address, global_host_page = fetch_global_addresses()
     try:
         print("\n\nChecking user host main version...")
-        current_version = get(f"{global_host_page}/current_user_host_main_version", timeout=10).text
+        response = get(f"{global_host_page}/current_user_host_main_version", timeout=15)
+        response.close()
+        current_version = response.text
+        if len(current_version) > 10 or current_version.count(".") != 2:
+            raise ZeroDivisionError
+
         if current_version == user_host_main_version:
             break
         else:
@@ -37,22 +43,28 @@ while True:
             version_split = user_host_main_version.split('.')
             if version_split[0] == current_version_split[0]:
                 if version_split[1] != current_version_split[1]:
-                    print(f"An recommended update is available. v{current_version}. Please download from github for important patches.\n https://github.com/BhaskarPanja93/Linkvertise-View-Bot-Client/releases")
+                    print(f"An recommended update is available. v{current_version}. Please download from github for important patches.\n https://github.com/BhaskarPanja93/View-Bot-Client/releases")
                     sleep(5)
+                    webbrowser.open("https://github.com/BhaskarPanja93/View-Bot-Client/releases", new=2)
                 else:
                     print(f"An optional update is available. v{current_version}")
-                    sleep(5)
+                    sleep(3)
+                    webbrowser.open("https://github.com/BhaskarPanja93/View-Bot-Client/releases", new=2)
                 break
             else:
-                print(f"User Host Main is too old to run. Please update to v{current_version} to continue!!\n https://github.com/BhaskarPanja93/Linkvertise-View-Bot-Client/releases")
+                print(f"User Host Main is too old to run. Please update to v{current_version} to continue!!\n https://github.com/BhaskarPanja93/View-Bot-Client/releases")
+                webbrowser.open("https://github.com/BhaskarPanja93/View-Bot-Client/releases", new=2)
                 input()
                 exit()
-    except:
-        print("Retrying...")
+    except Exception as e:
+        print(f"Retrying... : {repr(e)}")
+
+
+system_caller('cls')
+print("Version fetched...")
 
 
 ### Check local drive to use
-system_caller('cls')
 print('\n\nChecking directories...')
 local_drive_name = 'C'
 if not path.exists(f"{local_drive_name}://"):
@@ -63,11 +75,15 @@ if not path.exists(f"{local_drive_name}://"):
     else:
         local_drive_name = ''
         print('No available local drive, please create a github issue!!')
+        sleep(2)
+        webbrowser.open("https://github.com/BhaskarPanja93/View-Bot-Client/issues/new", new=2)
         input()
         exit()
 
+
 live_location = f"{local_drive_name}://viewbot_files"
 updates_location = f"{local_drive_name}://viewbot_files/updates"
+
 
 ### If first run, make necessary directories
 if not path.exists(live_location) or not path.exists(updates_location):
@@ -76,8 +92,12 @@ if not path.exists(live_location) or not path.exists(updates_location):
     with open(f'{live_location}/DO NOT MODIFY!!.txt', 'w') as file:
         file.write("Do not modify any file in this directory. It can cause conflicts and/or security bugs")
 
-### check user_host version
+
 system_caller('cls')
+print("Directories found...")
+
+
+### check user_host version
 try:
     with open(f'{live_location}/version', 'r') as version_info_file:
         version = float(version_info_file.read())
@@ -91,7 +111,7 @@ while True:
         print('This can take a while...')
         file_code = 'stable_user_host'
         global_host_address, global_host_page = fetch_global_addresses()
-        response = get(f"{global_host_page}/other_files?file_code={file_code}&version={version}", timeout=35).content
+        response = get(f"{global_host_page}/other_files?file_code={file_code}&version={version}", timeout=40).content
         if response[0] == 123 and response[-1] == 125:
             system_caller('cls')
             print("\n\nData received.")
@@ -108,10 +128,10 @@ while True:
                     print("\n\nNo new Updates")
                 break
             else:
-                _ = 1 / 0
+                raise ZeroDivisionError
         else:
-            _ = 1 / 0
-    except:
-        print("Retrying...")
+            raise ZeroDivisionError
+    except Exception as e:
+        print(f"Retrying... : {repr(e)}")
         sleep(1)
 Thread(target=system_caller, args=(f'{live_location}/user_host.exe',)).start()
